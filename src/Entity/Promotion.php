1522e1d2cb00eef60b1e08c9a768e2794e33e001
nicholasg2001 <nicholasgutierrez62001@gmail.com>
2024-07-24 19:09:05 -0400

    create find valid promotion query with Doctrine

--- src/Entity/Promotion.php
+++ src/Entity/Promotion.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PromotionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PromotionRepository::class)]
@@ -25,6 +27,13 @@ class Promotion
     #[ORM\Column]
     private array $criteria = [];
 
+    #[ORM\OneToMany(targetEntity: ProductPromotion::class, mappedBy: 'promotion')]
+    private Collection $productPromotions;
+
+    public function __construct()
+    {
+        $this->productPromotions = new ArrayCollection();
+    }
     public function getId(): ?int
     {
         return $this->id;
@@ -77,4 +86,22 @@ class Promotion
 
         return $this;
     }
+
+    /**
+     * @return Collection
+     */
+    public function getProductPromotions(): Collection
+    {
+        return $this->productPromotions;
+    }
+
+    /**
+     * @param Collection $productPromotions
+     */
+    public function setProductPromotions(Collection $productPromotions): void
+    {
+        $this->productPromotions = $productPromotions;
+    }
+
+
 }
