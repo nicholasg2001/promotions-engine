0c42c566ec32492536c8d191d4869311db3657bf
nicholasg2001 <nicholasgutierrez62001@gmail.com>
2024-07-24 15:17:05 -0400

    create serialization logic

--- src/DTO/LowestPriceEnquiry.php
+++ src/DTO/LowestPriceEnquiry.php
@@ -158,4 +158,8 @@ class LowestPriceEnquiry implements PromotionEnquiryInterface
     private ?int $promotionId;
     private ?string $promotionName;
 
+    public function jsonSerialize(): array
+    {
+        return get_object_vars($this);
+    }
 }
